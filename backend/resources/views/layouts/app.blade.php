d20f98eb8c775cd669e86c19dd38018add3b2858
Truonghocdot <truonghocdot05@gmail.com>
2026-03-01 14:31:44 +0700

    truonghd - fix

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -18,6 +18,10 @@
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:site_name" content="giabaotravel">
     <meta property="og:locale" content="vi_VN">
+    <!-- Fonts & Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
